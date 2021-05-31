32fb852a3c71cfa4e83dac74d84cc9e0fa274c74
dodo <do151015@gmail.com>
2021-05-31 16:27:41 +0900

    sql update

--- src/main/webapp/sql.sql
+++ src/main/webapp/sql.sql
@@ -1,6 +1,6 @@
 drop table member;
 
-CREATE TABLE member(	
+CREATE TABLE member(
 	mno    NUMBER CONSTRAINT member_mno_PK PRIMARY KEY NOT NULL,
     id    VARCHAR2(12) NOT NULL,
     password    VARCHAR2(20) NOT NULL,
@@ -16,6 +16,8 @@ CREATE TABLE member(
 
 select * from member;
 
+drop table display;
+
 CREATE TABLE display(
     dno    NUMBER CONSTRAINT display_dno_PK PRIMARY KEY NOT NULL,
     dname    VARCHAR2(50) NOT NULL,
@@ -24,12 +26,18 @@ CREATE TABLE display(
     loc    VARCHAR2(50) NOT NULL,
     poster    BLOB NOT NULL,
     tag    VARCHAR2(10) NOT NULL,
-    content    VARCHAR2 NOT NULL,
+    intro    VARCHAR2(500) NOT NULL,
+    detail_img  BLOB,
+    detail_txt VARCHAR2(1000),
     hours    DATE NOT NULL,
-    fee    NUMBER(6) NOT NULL,
+    fee    NUMBER(6),
+    fee_adult   NUMBER(6),
+    fee_teen    NUMBER(6),
+    fee_child   NUMBER(6),
+    discount    NUMBER(3),
     artist    VARCHAR2(20),
     tel    VARCHAR2(20),
-    home_pg    VARCHAR2,
+    home_pg    VARCHAR2(100),
     del    CHAR(1) default 'n' NOT NULL,
     mno    NUMBER constraint display_mno_fk references member NOT NULL
 );
@@ -54,9 +62,9 @@ CREATE TABLE review
 (
     rv_no    NUMBER CONSTRAINT review_rv_no_PK PRIMARY KEY NOT NULL,
     star_rate    NUMBER(1,1) NOT NULL,
-    content    VARCHAR2 NOT NULL,
+    content    VARCHAR2(500) NOT NULL,
     reg_date    DATE NOT NULL,
-    mno    NUMBER constraint review_mno_fk references member NOT NULL, 
+    mno    NUMBER constraint review_mno_fk references member NOT NULL,
     dno    NUMBER constraint review_dno_fk references display NOT NULL
 );
 
@@ -64,7 +72,7 @@ CREATE TABLE board
 (
     bno    NUMBER CONSTRAINT board_bno_PK PRIMARY KEY NOT NULL,
     title    VARCHAR2(50) NOT NULL,
-    content    VARCHAR2 NOT NULL,
+    content    VARCHAR2(4000) NOT NULL,
     reg_date    DATE NOT NULL,
     read_cnt    NUMBER default 0 NOT NULL,
     likes    NUMBER default 0 NOT NULL,
@@ -75,12 +83,12 @@ CREATE TABLE board
 CREATE TABLE reply
 (
     re_no    NUMBER CONSTRAINT reply_re_no_PK PRIMARY KEY NOT NULL,
-    content    VARCHAR2 NOT NULL,
+    content    VARCHAR2(500) NOT NULL,
     reg_date    DATE NOT NULL,
     ref    NUMBER NOT NULL,
     ref_level    NUMBER NOT NULL,
     ref_step    NUMBER NOT NULL,
     del    CHAR(1) default 'n' NOT NULL,
-    mno    NUMBER constraint review_mno_fk references member NOT NULL,
+    mno    NUMBER constraint reply_mno_fk references member NOT NULL,
     bno    NUMBER constraint reply_bno_fk references board NOT NULL
 );
