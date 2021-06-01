36d3a491cae19318809f11a4e8f29eb9767368a7
user <user@DESKTOP-CITFUM4>
2021-06-01 14:56:09 +0900

    sql 마지막 수정이었으면,,

--- src/main/webapp/sql.sql
+++ src/main/webapp/sql.sql
@@ -1,5 +1,5 @@
-drop table member;
 -- 회원
+drop table member;
 CREATE TABLE member
 (
 	mno    NUMBER CONSTRAINT member_mno_PK PRIMARY KEY NOT NULL,
@@ -8,17 +8,17 @@ CREATE TABLE member
     name    VARCHAR2(20) NOT NULL,
     nick_nm    VARCHAR2(20) NOT NULL,
     phone    VARCHAR2(20) NOT NULL,
-    gender    CHAR(2) default 'm' NOT NULL ,
+    gender    CHAR(2) default 'm' NOT NULL,
     age    NUMBER(3) NOT NULL,
     loc    VARCHAR2(12) NOT NULL,
-    del    CHAR(1) default 'n' NOT NULL ,
+    del    CHAR(1) default 'n' NOT NULL,
     reg_date    DATE NOT NULL
 );
 select * from member;
 
-drop table display;
 -- 전시
-CREATE TABLE display -- fee에 default 추가
+drop table display;
+CREATE TABLE display
 (
     dno    NUMBER CONSTRAINT display_dno_PK PRIMARY KEY NOT NULL,
     dname    VARCHAR2(50) NOT NULL,
@@ -31,11 +31,11 @@ CREATE TABLE display -- fee에 default 추가
     detail_img  BLOB,
     detail_txt VARCHAR2(1000),
     hours    DATE NOT NULL,
-    fee    NUMBER(6) default 0,
+    fee    NUMBER(6),
     fee_adult   NUMBER(6),
     fee_teen    NUMBER(6),
     fee_child   NUMBER(6),
-    discount    NUMBER(3),
+    discount    NUMBER(3) default 0,
     artist    VARCHAR2(20),
     tel    VARCHAR2(20),
     home_pg    VARCHAR2(100),
@@ -44,8 +44,8 @@ CREATE TABLE display -- fee에 default 추가
 );
 select * from display;
 
-drop table bookmark;
 -- 북마크
+drop table bookmark;
 CREATE TABLE bookmark
 (
     bm_no    NUMBER CONSTRAINT bookmark_bm_no_PK PRIMARY KEY NOT NULL,
@@ -54,35 +54,37 @@ CREATE TABLE bookmark
 );
 select * from bookmark;
 
-drop table reservation;
 -- 예매
+drop table reservation;
 CREATE TABLE reservation
 (
     rs_no    NUMBER CONSTRAINT reservation_rs_no_PK PRIMARY KEY NOT NULL,
-    cnt    NUMBER(4) NOT NULL,
+    cnt    NUMBER(4) default 0,
+    cnt_adult    NUMBER(3) default 0,
+    cnt_teen    NUMBER(3) default 0,
+    cnt_child    NUMBER(3) default 0,
     mno    NUMBER constraint reservation_mno_fk references member NOT NULL,
     dno    NUMBER constraint reservation_dno_fk references display NOT NULL
 );
 select * from reservation;
 
-drop table review;
-
 -- 한줄 리뷰
-CREATE TABLE review -- likes, del 칼럼 추가
+drop table review;
+CREATE TABLE review
 (
     rv_no    NUMBER CONSTRAINT review_rv_no_PK PRIMARY KEY NOT NULL,
     star_rate    NUMBER(1,1) NOT NULL,
     content    VARCHAR2(500) NOT NULL,
     reg_date    DATE NOT NULL,
-    likes    NUMBER default 0 NOT NULL,
+    likes    NUMBER default 0,
     del    CHAR(1) default 'n' NOT NULL,
     mno    NUMBER constraint review_mno_fk references member NOT NULL,
     dno    NUMBER constraint review_dno_fk references display NOT NULL
 );
 select * from review;
 
-drop table board;
 -- 게시판(스토리)
+drop table board;
 CREATE TABLE board
 (
     bno    NUMBER CONSTRAINT board_bno_PK PRIMARY KEY NOT NULL,
@@ -96,8 +98,8 @@ CREATE TABLE board
 );
 select * from board;
 
-drop table reply;
 -- 게시판에 댓글(답글 포함)
+drop table reply;
 CREATE TABLE reply
 (
     re_no    NUMBER CONSTRAINT reply_re_no_PK PRIMARY KEY NOT NULL,
